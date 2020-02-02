dbcfd47bd3454fac8675f14dd1ba0da697d75b3a
nikki <jl10018@nyu.edu>
2020-02-01 23:12:31 -0500

    init repo

---
+++ src/main/java/com/dailyblog/DailyblogApplication.java
@@ -0,0 +1,13 @@
+package com.dailyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailyblog/DailyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
