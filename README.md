be4a90c38a7b0f3defae499498f3d43a8cb21b47
nikki <jl10018@nyu.edu>
2020-02-05 21:12:29 -0500

    extract github config from code to properties && update README.md

--- README.md
+++ README.md
@@ -2,9 +2,27 @@
 Start date: 02/01/2020
 
 ## Refference
-[Spring documents](https://Spring.io/guides)    Main framework.   
-[Bootstrap](https://v3.bootcss.com/components)  Used to build a web.   
-[Github OAuth App](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)    Used to obtain login authorization from GitHub.  
+[Spring documents](https://Spring.io/guides): Main framework.   
+[Bootstrap](https://v3.bootcss.com/components): Used to build a web.   
+[Github OAuth App](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/): Used to obtain login authorization from GitHub.  
 
 ## Tools
-[Git](https://git-scm.com)   
+[Git](https://git-scm.com)    
+[Visual Paradigm](https://www.visual-paradigm.com/cn/): Used to draw mind maps.
+
+
+## Progress
+### Day 1   
+* 配置环境&emsp; Configure environment of IDEA.   
+* 尝试使用@Controller + Thymeleaf模板&emsp; Learn "@Controller" notation and "Thymeleaf" template.   
+* 创建Github仓库，使用Github进行版本控制&emsp; Create a new repository in Github to do version control.   
+* 学习使用Git工具&emsp; Learn about Git tools.   
+   
+### Day 2   
+* 分析目标成果&emsp; Analyze target achievement.
+* 使用Bootstrap的UI组件搭建导航栏&emsp; Use "Navbar" component of Bootstrap to build a navigation bar.   
+* 调用Github的API实现第三方授权登录&emsp; Build an OAuth App of Github.   
+
+### Day 3
+* 分离配置文件&emsp; Separate configuration files.
+* 
---
+++ src/main/java/com/dailyblog/controller/AuthorizeController.java
@@ -0,0 +1,39 @@
+package com.dailyblog.controller;
+
+import com.dailyblog.dto.AccessTokenDTO;
+import com.dailyblog.dto.GithubUser;
+import com.dailyblog.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientID;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientID);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
